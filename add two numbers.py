19b4bb7cc82fde17c645a219ecdf573dd2fe49ea
anukruti-2409 <65861873+anukruti-2409@users.noreply.github.com>
2020-06-17 21:07:59 +0530

    Add files via upload

---
+++ add two numbers.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 21:13:37 2020
+
+@author: ANUKRUTI DAS
+"""
+
+
+t=int(input())
+for i in range(t):
+    k=input().split()
+    print(int(k[0])+int(k[1]))
