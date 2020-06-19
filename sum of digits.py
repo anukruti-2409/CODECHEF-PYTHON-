d054fa2e4da496081a3795f11ab6579a1f09d4ff
anukruti-2409 <65861873+anukruti-2409@users.noreply.github.com>
2020-06-19 21:12:58 +0530

    Add files via upload

---
+++ sum of digits.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 19 21:18:25 2020
+
+@author: ANUKRUTI DAS
+"""
+
+
+t=int(input())
+
+for i in range(t):
+    y=input()
+    o=0
+    for x in y:
+        o+=int(x)
+    print(o)
